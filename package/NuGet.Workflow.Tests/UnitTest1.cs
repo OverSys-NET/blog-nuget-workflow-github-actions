5088a3790d77a7287310a2d053392f662d0cfccd
Lenin Paredes <lenin.paredes@oversys.net>
2023-08-29 16:59:38 -0400

    fix: class decoration code

--- package/NuGet.Workflow.Tests/UnitTest1.cs
+++ package/NuGet.Workflow.Tests/UnitTest1.cs
@@ -1,15 +1,23 @@
+//-----------------------------------------------------------------------
+// <copyright file="UnitTest1.cs" company="OverSys.NET">
+// Author: "Team OverSys.NET">
+// Copyright (c) OverSys.NET. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
 using NUnit.Framework;
 
-namespace NuGet.Workflow.Tests
-{
-   public class UnitTest1
-   {
+namespace NuGet.Workflow.Tests {
+
+   public class UnitTest1 {
+
+      #region methods
       [Test]
-      public void Test1()
-      {
-         var class1 = new Class1();
+      public void Test1() {
+         Class1 class1 = new Class1();
 
          Assert.That(class1.Value, Is.EqualTo("Foo"));
       }
+      #endregion
    }
+
 }
--- package/NuGet.Workflow/Class1.cs
+++ package/NuGet.Workflow/Class1.cs
@@ -1,7 +1,16 @@
-﻿namespace NuGet.Workflow
-{
-   public class Class1
-   {
+//-----------------------------------------------------------------------
+// <copyright file="Class1.cs" company="OverSys.NET">
+// Author: "Team OverSys.NET">
+// Copyright (c) OverSys.NET. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+namespace NuGet.Workflow {
+
+   public class Class1 {
+
+      #region declare
       public string Value { get; set; } = "Foo";
+      #endregion
    }
+
 }
